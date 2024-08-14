c06807a1e908f35d3b703ca4f6bab207c0e1b912
deepakdk11 <deepakkumar545424@gmail.com>
2024-08-14 19:41:57 +0530

    Init Project

---
+++ README.md
@@ -0,0 +1,1 @@
+#Assignment of Hubnine india private limited
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+  
+  return (
+    <div className="app">
+      <h1 className="text-xl text-red-700">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
